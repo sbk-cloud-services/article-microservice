3744d54694e70fd51cf7ce107ad6698e956eb734
leoyn <leo@mixify.ga>
2021-03-10 19:35:02 +0100

    Add kubernetes configs & automated build

--- src/main/java/de/leuphana/shop/articlemicroservice/connector/ArticleServiceApplication.java
+++ src/main/java/de/leuphana/shop/articlemicroservice/connector/ArticleServiceApplication.java
@@ -2,11 +2,13 @@ package de.leuphana.shop.articlemicroservice.connector;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import de.leuphana.shop.articlemicroservice.component.behaviour.ArticleServiceImplementation;
 
+@SpringBootApplication
 public class ArticleServiceApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(ArticleServiceApplication.class, args);
@@ -15,7 +17,7 @@ public class ArticleServiceApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationcontext-jpa-connector.xml");
-        ArticleDatabaseConnector articleDatabaseConnector = (ArticleDatabaseConnector) applicationContext.getBean("customerDatabaseConnector");
+        ArticleDatabaseConnector articleDatabaseConnector = (ArticleDatabaseConnector) applicationContext.getBean("articleDatabaseConnector");
         ArticleServiceImplementation.getInstance().setArticleDatabaseConnector(articleDatabaseConnector);
     }
 }
